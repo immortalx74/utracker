f9d8909dffc489f1276ecf769e0bbfcee330b7ae
immortalx74 <jfdodis@yahoo.com>
2019-07-27 09:02:15 +0300

    project

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set path=C:\dev\tracker2;%path%
+set path=C:\dev\tracker;%path%
 
 set SOURCES=imgui/imgui.cpp imgui/imgui_draw.cpp imgui/imgui_widgets.cpp imgui/imgui-SFML.cpp imgui/imgui_demo.cpp main.cpp
 
-start /wait /B cl /MP /Fdbuild\ /Febuild/tracker /Fobuild\  /EHsc /Zi /FC /nologo /W1 /Od /Iinclude /Iimgui %SOURCES% /link lib\sfml-system.lib lib\sfml-graphics.lib lib\sfml-main.lib lib\sfml-window.lib opengl32.lib winmm.lib gdi32.lib lib\freetype.lib lib\fmodL_vc.lib
+start /wait /B cl /MP /Fdbuild\ /Febuild/tracker /Fobuild\  /EHsc /Zi /FC /nologo /W1 /Od /Iinclude /Iimgui %SOURCES% /link SUBSYSTEM:CONSOLE lib\sfml-system.lib lib\sfml-graphics.lib lib\sfml-main.lib lib\sfml-window.lib opengl32.lib winmm.lib gdi32.lib lib\freetype.lib lib\fmodL_vc.lib
 
 
 start /wait build/tracker.exe
